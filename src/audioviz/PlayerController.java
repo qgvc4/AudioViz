4b489da4e97db0f28ef48a4861a3bc07cba09fce
qgvc4 <qgvc4@mail.missouri.edu>
2018-02-03 16:11:43 -0600

    add listener

--- src/audioviz/PlayerController.java
+++ src/audioviz/PlayerController.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
@@ -16,6 +18,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
+import javafx.scene.input.MouseDragEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -28,60 +31,59 @@ import javafx.util.Duration;
 /**
  * FXML Controller class
  *
- * @author dale
- * Music: http://www.bensound.com/royalty-free-music
+ * @author dale Music: http://www.bensound.com/royalty-free-music
  * http://www.audiocheck.net/testtones_sinesweep20-20k.php
  * http://stackoverflow.com/questions/11994366/how-to-reference-primarystage
  */
 public class PlayerController implements Initializable {
-    
+
     @FXML
     private AnchorPane vizPane;
-    
+
     @FXML
     private MediaView mediaView;
-    
+
     @FXML
     private Text filePathText;
-    
+
     @FXML
     private Text lengthText;
-    
+
     @FXML
     private Text currentText;
-    
+
     @FXML
     private Text bandsText;
-    
+
     @FXML
     private Text visualizerNameText;
-    
+
     @FXML
     private Text errorText;
-    
+
     @FXML
     private Menu visualizersMenu;
-    
+
     @FXML
     private Menu bandsMenu;
-    
+
     @FXML
     private Slider timeSlider;
-    
+
     private Media media;
     private MediaPlayer mediaPlayer;
-    
+
     private Integer numBands = 40;
     private final Double updateInterval = 0.05;
-    
+
     private ArrayList<Visualizer> visualizers;
     private Visualizer currentVisualizer;
     private final Integer[] bandsList = {1, 2, 4, 8, 16, 20, 40, 60, 100, 120, 140};
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         bandsText.setText(Integer.toString(numBands));
-        
+
         visualizers = new ArrayList<>();
         visualizers.add(new EllipseVisualizer1());
         visualizers.add(new EllipseVisualizer2());
@@ -96,7 +98,7 @@ public class PlayerController implements Initializable {
         }
         currentVisualizer = visualizers.get(0);
         visualizerNameText.setText(currentVisualizer.getName());
-        
+
         for (Integer bands : bandsList) {
             MenuItem menuItem = new MenuItem(Integer.toString(bands));
             menuItem.setUserData(bands);
@@ -105,17 +107,26 @@ public class PlayerController implements Initializable {
             });
             bandsMenu.getItems().add(menuItem);
         }
+        // add listener to slider
+        timeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
+                if (mediaPlayer != null) {
+                    mediaPlayer.seek(Duration.seconds(timeSlider.getValue() / 1000));
+                }
+            }
+        });
     }
-    
+
     private void selectVisualizer(ActionEvent event) {
-        MenuItem menuItem = (MenuItem)event.getSource();
-        Visualizer visualizer = (Visualizer)menuItem.getUserData();
+        MenuItem menuItem = (MenuItem) event.getSource();
+        Visualizer visualizer = (Visualizer) menuItem.getUserData();
         changeVisualizer(visualizer);
     }
-    
+
     private void selectBands(ActionEvent event) {
-        MenuItem menuItem = (MenuItem)event.getSource();
-        numBands = (Integer)menuItem.getUserData();
+        MenuItem menuItem = (MenuItem) event.getSource();
+        numBands = (Integer) menuItem.getUserData();
         if (currentVisualizer != null) {
             currentVisualizer.start(numBands, vizPane);
         }
@@ -124,7 +135,7 @@ public class PlayerController implements Initializable {
         }
         bandsText.setText(Integer.toString(numBands));
     }
-    
+
     private void changeVisualizer(Visualizer visualizer) {
         if (currentVisualizer != null) {
             currentVisualizer.end();
@@ -133,15 +144,15 @@ public class PlayerController implements Initializable {
         currentVisualizer.start(numBands, vizPane);
         visualizerNameText.setText(currentVisualizer.getName());
     }
-    
+
     private void openMedia(File file) {
         filePathText.setText("");
         errorText.setText("");
-        
+
         if (mediaPlayer != null) {
             mediaPlayer.dispose();
         }
-        
+
         try {
             media = new Media(file.toURI().toString());
             mediaPlayer = new MediaPlayer(media);
@@ -162,7 +173,7 @@ public class PlayerController implements Initializable {
             errorText.setText(ex.toString());
         }
     }
-    
+
     private void handleReady() {
         Duration duration = mediaPlayer.getTotalDuration();
         lengthText.setText(duration.toString());
@@ -172,50 +183,57 @@ public class PlayerController implements Initializable {
         timeSlider.setMin(0);
         timeSlider.setMax(duration.toMillis());
     }
-    
+
     private void handleEndOfMedia() {
         mediaPlayer.stop();
         mediaPlayer.seek(Duration.ZERO);
         timeSlider.setValue(0);
     }
-    
+
     private void handleUpdate(double timestamp, double duration, float[] magnitudes, float[] phases) {
         Duration ct = mediaPlayer.getCurrentTime();
         double ms = ct.toMillis();
         currentText.setText(Double.toString(ms));
-        timeSlider.setValue(ms);
-        
+        if (ms % 1000 == 0) {
+            timeSlider.setValue(ms);
+        }
         currentVisualizer.update(timestamp, duration, magnitudes, phases);
     }
-    
+
     @FXML
     private void handleOpen(Event event) {
-        Stage primaryStage = (Stage)vizPane.getScene().getWindow();
+        Stage primaryStage = (Stage) vizPane.getScene().getWindow();
         FileChooser fileChooser = new FileChooser();
         File file = fileChooser.showOpenDialog(primaryStage);
         if (file != null) {
             openMedia(file);
         }
     }
-    
+
     @FXML
     private void handlePlay(ActionEvent event) {
         if (mediaPlayer != null) {
             mediaPlayer.play();
         }
     }
-    
+
     @FXML
     private void handlePause(ActionEvent event) {
         if (mediaPlayer != null) {
-           mediaPlayer.pause(); 
+            mediaPlayer.pause();
         }
     }
-    
+
     @FXML
     private void handleStop(ActionEvent event) {
         if (mediaPlayer != null) {
-           mediaPlayer.stop(); 
+            mediaPlayer.stop();
         }
     }
+    
+//    @Override
+//    public String toString() {
+//        DecimalFormat format = new DecimalFormat("#.0");
+//        return 
+//    }
 }
